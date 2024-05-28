923b9b0e9e5dfae06d41ffabb15331b044069961
JennyDisc <touki741@hotmail.com>
2024-05-28 19:42:00 +0800

    移除註解

--- service/uploadImages.js
+++ service/uploadImages.js
@@ -6,15 +6,12 @@ const upload = multer({
         // 圖片檔案不超過2MB
         fileSize: 2 * 1024 * 1024,
     },
-    // fileFilter(req, file, cb, next) {
     fileFilter(req, file, next) {
         const ext = path.extname(file.originalname).toLowerCase();
         // 限定上傳的圖片格式
         if (ext !== '.jpg' && ext !== '.png' && ext !== '.jpeg') {
-            // cb(new Error("檔案格式錯誤，僅限上傳 jpg、jpeg 與 png 格式。"));
             next(appError(400, "檔案格式錯誤，僅限上傳 jpg、jpeg 與 png 格式。"));
         }
-        // cb(null, true);
         next(null, true);
     },
 }).any();
